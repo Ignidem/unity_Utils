4256eb81bf0c697a95ff59f886a24784bed1c715
Eivlisskiv <gautma13@gmail.com>
2022-05-08 11:21:26 -0400

    init;

---
+++ Vectors/Vectors2.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace IgnitedBox.UnityUtilities.Vectors
+{
+    public static class Vectors2
+    {
+        public static Vector2 FromDegAngle(float angle, float magnitude = 1)
+            => FromRadAngle(angle * Mathf.Deg2Rad, magnitude);
+        public static Vector2 FromRadAngle(float angle, float magnitude = 1)
+            => new Vector2(Mathf.Cos(angle) * magnitude, Mathf.Sin(angle) * magnitude);
+
+        public static Vector2 Rotate(this Vector2 vect, float angle)
+            => Quaternion.Euler(0, 0, angle) * vect;
+
+        public static float TrueAngle(this Vector2 from)
+            => -TrueAngle(from, Vector2.right);
+
+        public static float TrueAngle(Vector2 from, Vector2 to)
+            => Vector2.Angle(from, to) * (from.y > to.y ? -1 : 1);
+
+        public static float WorldAngle(this Vector2 from, Vector2 to)
+        {
+            int y = from.y > to.y ? -1 : 1;
+            return Vector2.Angle(Vector2.right, to - from) * y;
+        }
+    }
+
+    [System.Serializable]
+    public class SerializableFloat2
+    {
+        public static implicit operator Vector2(SerializableFloat2 v)
+            => new Vector2(v.x, v.y);
+        public static implicit operator SerializableFloat2(Vector2 v)
+            => new SerializableFloat2(v.x, v.y);
+
+        public float x;
+        public float y;
+
+        public SerializableFloat2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static SerializableFloat2 operator *(SerializableFloat2 a, float b)
+            => new SerializableFloat2(a.x * b, a.y * b);
+    }
+
+    [System.Serializable]
+    public class SerializableInt2
+    {
+        public static implicit operator Vector2Int(SerializableInt2 v)
+            => new Vector2Int(v.x, v.y);
+        public static implicit operator SerializableInt2(Vector2Int v)
+            => new SerializableInt2(v.x, v.y);
+
+        public int x;
+        public int y;
+
+        public SerializableInt2() { }
+
+        public SerializableInt2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Vectors/Vectors3.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace IgnitedBox.UnityUtilities.Vectors
+{
+    public static class Vectors3
+    {
+        public static Vector3 Div(this Vector3 a, Vector3 b)
+            => new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+        public static Vector3 Mult(this Vector3 a, Vector3 b)
+            => new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+
+    [System.Serializable]
+    public struct SerializableFloat3
+    {
+        public static implicit operator Vector3(SerializableFloat3 v)
+            => new Vector3(v.x, v.y, v.z);
+        public static implicit operator SerializableFloat3(Vector3 v)
+            => new SerializableFloat3(v.x, v.y, v.z);
+
+        public float x;
+        public float y;
+        public float z;
+
+        public SerializableFloat3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    [System.Serializable]
+    public struct SerializableInt3
+    {
+        public static implicit operator Vector3Int(SerializableInt3 v)
+            => new Vector3Int(v.x, v.y, v.z);
+        public static implicit operator SerializableInt3(Vector3Int v)
+            => new SerializableInt3(v.x, v.y, v.z);
+
+        public int x;
+        public int y;
+        public int z;
+
+        public SerializableInt3(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
