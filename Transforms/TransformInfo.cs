75f0e84a50f89eb213883855ac03f80253200e5e
Eivlisskiv <gautma13@gmail.com>
2023-08-09 15:48:22 -0400

    transform info

---
+++ Transforms/TransformInfo.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Assets.External.unity_utils.Transforms
+{
+	public struct TransformInfo
+	{
+		public static implicit operator TransformInfo(Transform transform)
+		{
+			return new TransformInfo()
+			{
+				position = transform.position,
+				rotation = transform.rotation.eulerAngles,
+				scale = transform.lossyScale,
+			};
+		}
+
+		public Vector3Info position;
+		public Vector3Info rotation;
+		public Vector3Info scale;
+
+		public void Apply(Transform transform)
+		{
+			ApplyPosition(transform);
+			ApplyRotation(transform);
+		}
+
+		public void ApplyPosition(Transform transform)
+		{
+			switch (position.space)
+			{
+				case Space.World:
+					transform.position = position;
+					break;
+				case Space.Self:
+					transform.localPosition = position;
+					break;
+			}
+		}
+
+		public void ApplyRotation(Transform transform)
+		{
+			Quaternion rot = Quaternion.Euler(rotation);
+			switch (rotation.space)
+			{
+				case Space.World:
+					transform.rotation = rot;
+					break;
+				case Space.Self:
+					transform.localRotation = rot;
+					break;
+			}
+		}
+
+		public void ApplyScale(Transform transform)
+		{
+			switch (scale.space)
+			{
+				case Space.World:
+					transform.localScale = scale;
+					break;
+				case Space.Self:
+					transform.localScale = scale;
+					break;
+			}
+		}
+	}
+}
---
+++ Transforms/Vector3Info.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.External.unity_utils.Transforms
+{
+	public struct Vector3Info
+	{
+		public static implicit operator Vector3Info(Vector3 vect)
+		{
+			return new Vector3Info()
+			{
+				vector = vect,
+				space = Space.World,
+			};
+		}
+
+		public static implicit operator Vector3(Vector3Info info) => info.vector;
+
+		public Vector3 vector;
+		public Space space;
+	}
+}
