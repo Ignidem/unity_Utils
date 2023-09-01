5f0b9c5c1fbaa7394a45825ee80d585a3385d7a5
Eivlisskiv <gautma13@gmail.com>
2023-09-01 17:32:51 -0400

    input drop handler;

---
+++ Inputs/IDropHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace UnityUtils.Inputs
+{
+	public interface IDropHandler<T>
+	{
+		bool OnDrop(T target);
+	}
+
+	public interface IDropHandlerAsync<T>
+	{
+		Task<bool> OnDrop(T target);
+	}
+}
---
+++ Inputs/InputsEx.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityUtils.Inputs
+{
+	public static class InputsEx
+	{
+		public static bool TryRaycast(this Camera camera, Vector2 screenPosition, out RaycastHit hit)
+		{
+			if (!camera)
+			{
+				if (!Camera.main)
+				{
+					hit = default;
+					return false;
+				}
+
+				camera = Camera.main;
+			}
+
+			Ray ray = camera.ScreenPointToRay(screenPosition);
+
+			return Physics.Raycast(ray, out hit);
+		}
+
+		public static bool TryRaycast<T>(this Camera camera, Vector2 screenPosition, out RaycastHit hit, out T target)
+		{
+			if (!camera.TryRaycast(screenPosition, out hit))
+			{
+				target = default;
+				return false;
+			}
+
+			return hit.collider.gameObject.TryGetComponent(out target);
+		}
+	
+		public static bool Drop<T>(this T item, Vector2 pointerScreenPosition, Camera camera = null)
+		{
+			if (!camera.TryRaycast(pointerScreenPosition, out _, out IDropHandler<T> handler))
+				return false;
+
+			return handler.OnDrop(item);
+		}
+
+		public static async Task<bool> DropAsync<T>(this T item, Vector2 pointerScreenPosition, Camera camera = null)
+		{
+			if (!camera.TryRaycast(pointerScreenPosition, out _, out IDropHandlerAsync<T> handler))
+				return false;
+
+			return await handler.OnDrop(item);
+		}
+	}
+}
