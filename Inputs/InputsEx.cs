2b8c781984f60a700babf6d9d835ff88be43cac3
Eivlisskiv <gautma13@gmail.com>
2023-09-02 21:45:11 -0400

    raycast option;

--- Inputs/InputsEx.cs
+++ Inputs/InputsEx.cs
@@ -5,7 +5,7 @@ namespace UnityUtils.Inputs
 {
 	public static class InputsEx
 	{
-		public static bool TryRaycast(this Camera camera, Vector2 screenPosition, out RaycastHit hit)
+		public static bool TryRaycast(this Camera camera, RaycastOptions options, out RaycastHit hit)
 		{
 			if (!camera)
 			{
@@ -18,14 +18,14 @@ namespace UnityUtils.Inputs
 				camera = Camera.main;
 			}
 
-			Ray ray = camera.ScreenPointToRay(screenPosition);
+			Ray ray = camera.ScreenPointToRay(options.screenPosition);
 
-			return Physics.Raycast(ray, out hit);
+			return Physics.Raycast(ray, out hit, options.maxDistance, options.layerMask);
 		}
 
-		public static bool TryRaycast<T>(this Camera camera, Vector2 screenPosition, out RaycastHit hit, out T target)
+		public static bool TryRaycast<T>(this Camera camera, RaycastOptions options, out RaycastHit hit, out T target)
 		{
-			if (!camera.TryRaycast(screenPosition, out hit))
+			if (!camera.TryRaycast(options, out hit))
 			{
 				target = default;
 				return false;
@@ -34,17 +34,17 @@ namespace UnityUtils.Inputs
 			return hit.collider.gameObject.TryGetComponent(out target);
 		}
 	
-		public static bool Drop<T>(this T item, Vector2 pointerScreenPosition, Camera camera = null)
+		public static bool Drop<T>(this T item, RaycastOptions options, Camera camera = null)
 		{
-			if (!camera.TryRaycast(pointerScreenPosition, out _, out IDropHandler<T> handler))
+			if (!camera.TryRaycast(options, out _, out IDropHandler<T> handler))
 				return false;
 
 			return handler.OnDrop(item);
 		}
 
-		public static async Task<bool> DropAsync<T>(this T item, Vector2 pointerScreenPosition, Camera camera = null)
+		public static async Task<bool> DropAsync<T>(this T item, RaycastOptions options, Camera camera = null)
 		{
-			if (!camera.TryRaycast(pointerScreenPosition, out _, out IDropHandlerAsync<T> handler))
+			if (!camera.TryRaycast(options, out _, out IDropHandlerAsync<T> handler))
 				return false;
 
 			return await handler.OnDrop(item);
---
+++ Inputs/RaycastOptions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Inputs
+{
+	public struct RaycastOptions
+	{
+		public static implicit operator RaycastOptions(Vector2 pos) => new RaycastOptions(pos);
+
+		public Vector3 screenPosition;
+		public int maxDistance;
+		public LayerMask layerMask;
+
+		public RaycastOptions(Vector2 screenPosition, LayerMask? layerMask = null, int? maximumDistance = null)
+		{
+			this.screenPosition = screenPosition;
+			maxDistance = maximumDistance ?? int.MaxValue;
+			this.layerMask = layerMask ?? 0;
+		}
+	}
+}
