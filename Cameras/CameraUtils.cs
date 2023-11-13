1fcdb0867e3f506e0a8795fcb86ea51b40bdbe5b
Eivlisskiv <gautma13@gmail.com>
2023-11-13 18:19:22 -0500

    camera utils - get view rect at distance;

---
+++ Cameras/CameraUtils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Cameras
+{
+	public static class CameraUtils
+	{
+		public static Vector2 SizeAtDistance(this Camera cam, float distance)
+		{
+			Transform transform = cam.transform;
+			Vector3 forward = transform.forward;
+
+			Ray max = cam.ViewportPointToRay(new Vector2(1, 1), Camera.MonoOrStereoscopicEye.Mono);
+			Ray min = cam.ViewportPointToRay(new Vector2(0, 0), Camera.MonoOrStereoscopicEye.Mono);
+
+			float angle = Vector3.Angle(min.direction, forward);
+			float d = (distance - cam.nearClipPlane) / Mathf.Cos(angle * Mathf.Deg2Rad);
+
+			Vector3 maxPoint = max.GetPoint(d);
+			Vector3 minPoint = min.GetPoint(d);
+
+			Vector3 vect = Vector3.ProjectOnPlane(maxPoint - minPoint, forward);
+			return new Vector2(vect.x, vect.z);
+		}
+	}
+}
