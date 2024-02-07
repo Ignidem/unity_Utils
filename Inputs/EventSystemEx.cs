002a345af4683eff04f34a39aadf80de75f43c15
Eivlisskiv <gautma13@gmail.com>
2024-02-07 04:01:41 -0500

    event system ex;

---
+++ Inputs/EventSystemEx.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityUtils.Inputs
+{
+	public static class EventSystemEx
+	{
+		public static void Select<T>(this T component, BaseEventData data = null)
+			where T : Component, ISelectHandler
+		{
+			EventSystem current = EventSystem.current;
+			if (!current) 
+				return;
+
+			current.SetSelectedGameObject(component.gameObject, data);
+		}
+
+		public static void Deselect(EventSystem system)
+		{
+			system.SetSelectedGameObject(null);
+		}
+	}
+}
