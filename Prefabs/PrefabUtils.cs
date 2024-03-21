e23178140d445ffd0037655b1847df0a6c388801
Eivlisskiv <gautma13@gmail.com>
2024-03-21 18:52:23 -0400

    prefab utils methods;

--- Prefabs/PrefabUtils.cs
+++ Prefabs/PrefabUtils.cs
@@ -4,11 +4,20 @@ namespace  UnityUtils.Prefabs
 {
 	public static class PrefabUtils
 	{
-		public static bool IsPrefab(this GameObject obj)
+		public static bool IsPrefab(this Object obj)
 		{
-			return !obj.scene.IsValid();
+			return obj is GameObject go ? !go.scene.IsValid() : false;
 		}
 
 		public static bool IsPrefab(this Component comp) => comp.gameObject.IsPrefab();
+
+		public static Object GetPrefab(this Object obj) 
+		{
+#if UNITY_EDITOR
+			return UnityEditor.PrefabUtility.GetCorrespondingObjectFromOriginalSource(obj);
+#else
+			throw new System.NotImplementedException();
+#endif
+		}
 	}
 }
