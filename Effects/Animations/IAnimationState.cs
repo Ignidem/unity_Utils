328873a90158122128a75b1c0a1484161221eacb
Eivlisskiv <gautma13@gmail.com>
2024-05-06 03:01:11 -0400

    cleanup animation states

--- Effects/Animations/IAnimationState.cs
+++ Effects/Animations/IAnimationState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 namespace UnityUtils.Animations.StateListener
 {
+	public delegate void AnimationEventListener(IAnimationState state, AnimationEvent evnt);
+
 	public interface IAnimationState
 	{
 		string StateName { get; }
@@ -11,59 +13,19 @@ namespace UnityUtils.Animations.StateListener
 		bool IsPlaying { get; }
 		float Length { get; }
 		int Layer { get; }
+		float Time { get; }
+
+		event AnimationEventListener OnEvent;
 
 		void Play(float blendTime = 0.1f);
 		void Stop(float blendTime = 0.1f);
+		void Pause();
+		void Resume();
+
 		TaskAwaiter GetAwaiter();
 		async Task GetTask()
 		{
 			await this;
-		}
-	}
-
-	public readonly struct AnimationState : IAnimationState
-	{
-		public string StateName { get; }
-		public int Id { get; }
-		public readonly bool IsPlaying => state.enabled;
-		public float Length => state.length;
-		public int Layer => state.layer;
-
-		private readonly Animator animator;
-		private readonly UnityEngine.AnimationState state;
-
-		public AnimationState(Animator animator, UnityEngine.AnimationState state)
-		{
-			this.animator = animator;
-			this.state = state;
-			StateName = state.name;
-			Id = Animator.StringToHash(StateName);
-			this.state = state;
-		}
-
-		public TaskAwaiter GetAwaiter()
-		{
-			if (!IsPlaying)
-				return Task.CompletedTask.GetAwaiter();
-			int time = (int)((state.length - state.time) * 1000);
-			return Task.Delay(time).GetAwaiter();
-		}
-
-		public void Play(float blendTime = 0.1F)
-		{
-			if (blendTime > 0)
-			{
-				animator.CrossFade(Id, blendTime, Layer);
-			}
-			else
-			{
-				animator.Play(Id, Layer);
-			}
-		}
-
-		public void Stop(float blendTime = 0.1F)
-		{
-			throw new System.NotImplementedException();
 		}
 	}
 }
--- Effects/Animations/Mechanim/AnimatorEx.cs
+++ Effects/Animations/Mechanim/AnimatorEx.cs
@@ -23,29 +23,6 @@ namespace UnityUtils.Effects.Animations.Mechanim
 			}
 		}
 
-		public static IAnimationState[] GetStates<T>(this Animator animator)
-			where T : struct, IPlayable
-		{
-			PlayableGraph graph = animator.playableGraph;
-			Playable controller = animator.GetAnimatorPlayable();
-			Playable layers = controller.GetInputs().FirstOrDefault(
-				input => input.GetPlayableType() == typeof(AnimationLayerMixerPlayable)
-			);
-			int layerCount = layers.GetInputCount();
-			List<IAnimationState> states = new List<IAnimationState>();
-
-			for (int l = 0; l < layerCount; l++)
-			{
-				var layer = layers.GetInput(l);
-				foreach (var clip in layer.GetInputs<AnimationClipPlayable>(true))
-				{
-					states.Add(PlayableState.Create(graph, clip, l));
-				}
-			}
-
-			return states.ToArray();
-		}
-
 		public static Playable GetAnimatorPlayable(this Animator animator)
 		{
 			PlayableGraph graph = animator.playableGraph;
