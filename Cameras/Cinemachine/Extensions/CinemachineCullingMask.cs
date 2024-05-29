b8375994fb491474fbad48f13f26ff17085bd424
Eivlisskiv <gautma13@gmail.com>
2024-05-28 20:58:51 -0400

    cinemachine utils

---
+++ Cameras/Cinemachine/Extensions/CinemachineCullingMask.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+namespace UnityUtils.Cameras.Cinemachine.Extensions
+{
+	public class CinemachineCullingMask : CinemachineExtension
+	{
+		[SerializeField]
+		private LayerMask cullMask;
+
+		public override bool OnTransitionFromCamera(ICinemachineCamera fromCam, Vector3 worldUp, float deltaTime)
+		{
+			UpdateCullingMask();
+			return base.OnTransitionFromCamera(fromCam, worldUp, deltaTime);
+		}
+
+		private void UpdateCullingMask()
+		{
+			CinemachineBrain brain = CinemachineCore.FindPotentialTargetBrain(ComponentOwner);
+			Camera camera = brain.OutputCamera;
+			camera.cullingMask = cullMask;
+		}
+
+		protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, 
+			CinemachineCore.Stage stage, ref CameraState state, float deltaTime) { }
+	}
+}
