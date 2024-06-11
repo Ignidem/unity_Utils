83fc79d21fe1bbf56524470d93013e9f79af0b5c
Eivlisskiv <gautma13@gmail.com>
2024-06-11 18:05:03 -0400

    animation event broadcaster

---
+++ Effects/Animations/IAnimationEventBroadcaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using UnityUtils.Animations.AnimationEvents;
+
+namespace UnityUtils.Animations.StateListener
+{
+	public delegate void AnimationEventDelegate(string subEvent, IAnimationEventInfo evnt);
+
+	public interface IAnimationEventBroadcaster
+	{
+		public event AnimationEventDelegate OnEvent;
+		public void Broadcast(string subEvent, IAnimationEventInfo info);
+		Task<bool> OnEventAsync(string eventName, float offset);
+	}
+}
--- Effects/Animations/IAnimationState.cs
+++ Effects/Animations/IAnimationState.cs
@@ -1,12 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityUtils.Animations.AnimationEvents;
 
 namespace UnityUtils.Animations.StateListener
 {
-	public delegate void AnimationEventListener(string subEvent, IAnimationEventInfo evnt);
-
 	public interface IAnimationState
 	{
 		string StateName { get; }
@@ -19,8 +16,7 @@ namespace UnityUtils.Animations.StateListener
 		float NormalizedTime { get; }
 		float RemainingTime => Length - Time;
 
-		event AnimationEventListener OnEvent;
-		Task<bool> OnEventAsync(string eventName, float offset);
+		public IAnimationEventBroadcaster EventBroadcaster { get; set; }
 
 		void Play(float blendTime = 0.1f);
 		void Stop(float blendTime = 0.1f);
