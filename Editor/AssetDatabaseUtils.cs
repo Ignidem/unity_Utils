3174bbeecf1527f0720307839eb34220ce369fae
Eivlisskiv <gautma13@gmail.com>
2024-07-20 14:29:02 -0400

    mesh saving

---
+++ Editor/AssetDatabaseUtils.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Editor
+{
+	public static class AssetDatabaseUtils
+	{
+		public static string ToProjectPath(this string devicePath)
+		{
+			string projectRootPath = Application.dataPath;
+			int k = projectRootPath.LastIndexOf('/') + 1;
+
+			string path = devicePath.StartsWith(projectRootPath) ? devicePath[k..] : devicePath;
+
+			string dataFolder = projectRootPath[k..];
+			if (!path.StartsWith(dataFolder))
+				throw new System.Exception("Path is not in the project folder \n" + path);
+
+			return path;
+		}
+	}
+}
---
+++ Editor/ContextMenus/MeshFilterPropertyContextMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityUtils.Editor;
+using UnityUtils.Editor.ContextMenus;
+
+namespace UnityUtils.Effects.VisualEffects.Editor
+{
+	[PropertyContextMenu(typeof(Mesh))]
+	public class MeshPropertyContextMenu : IPropertyContextMenu
+	{
+		public void AddPropertyContextMenu(GenericMenu menu, SerializedProperty property)
+		{
+			if (property.objectReferenceValue is not Mesh mesh || !mesh)
+				return;
+
+			menu.AddItem(new GUIContent("Save Mesh"), false, () => SaveMesh(mesh));
+		}
+
+		private void SaveMesh(Mesh mesh)
+		{
+			MeshUtility.Optimize(mesh);
+
+			var obj = Selection.activeObject;
+			string path = obj ? AssetDatabase.GetAssetPath(obj) : "Assets";
+			path = EditorUtility.SaveFilePanel("Save " + mesh.name, path, mesh.name, "mesh");
+
+			if (string.IsNullOrEmpty(path))
+				return;
+
+			AssetDatabase.CreateAsset(mesh, path.ToProjectPath());
+		}
+	}
+}
