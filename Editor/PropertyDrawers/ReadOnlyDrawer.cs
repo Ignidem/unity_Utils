a485b421d5f5f7bc55c66ad78c58d42dc0fa2083
Eivlisskiv <gautma13@gmail.com>
2025-02-02 20:14:54 -0500

    added toggalable read only property attribute for editor

---
+++ Editor/PropertyDrawers/ReadOnlyDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityUtils.PropertyAttributes;
+using UnityUtils.RectUtils;
+
+namespace UnityUtils.Editor.PropertyDrawers
+{
+	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+	public class ReadOnlyDrawer : ExtendedPropertyDrawer
+	{
+		protected override LabelDrawType LabelType => LabelDrawType.None;
+
+		private bool isUnlocked;
+
+		protected override float DrawProperty(ref Rect position, SerializedProperty property, GUIContent label)
+		{
+			ReadOnlyAttribute attr = (ReadOnlyAttribute)attribute;
+			float xOffset = 0;
+			if (attr.hasToggle)
+			{
+				Rect unlockRect = position.SetSize(LineHeight, LineHeight);
+				isUnlocked = EditorGUI.Toggle(unlockRect, isUnlocked);
+				xOffset = LineHeight;
+			}
+
+			EditorGUI.BeginDisabledGroup(!isUnlocked);
+			EditorGUI.PropertyField(position.MoveX(xOffset), property);
+			EditorGUI.EndDisabledGroup();
+
+			return 0;
+		}
+	}
+}
---
+++ PropertyAttributes/ReadOnlyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.PropertyAttributes
+{
+	public class ReadOnlyAttribute : PropertyAttribute 
+	{
+		public readonly bool hasToggle;
+		public ReadOnlyAttribute(bool hasToggle = false) 
+		{
+			this.hasToggle = hasToggle;
+		}
+	}
+}
