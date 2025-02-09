a92e26c0ac25485df357ee8cb7f25333257e862a
Eivlisskiv <gautma13@gmail.com>
2025-02-09 03:33:58 -0500

    contained class for component and its grouping container object & added loading coroutine for text component

---
+++ GameObjects/Contained.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.GameObjects
+{
+	[System.Serializable]
+	public class Contained<T>
+		where T : UnityEngine.Object
+	{
+		public static implicit operator Transform(Contained<T> cnt) => cnt.Container;
+		public static implicit operator T(Contained<T> cnt) => cnt.Target;
+
+		[field: SerializeField] public Transform Container { get; private set; }
+		[field: SerializeField] public T Target { get; private set; }
+
+		public bool ContainerActive
+		{
+			get => Container.gameObject.activeSelf;
+			set => Container.gameObject.SetActive(value);
+		}
+	}
+}
---
+++ UI/TMPPro/TextUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.External.unity_utils.UI.TMPPro
+{
+	public static class TextUtils
+	{
+		public static Coroutine LoadingText(this TMP_Text label, string content)
+		{
+			return label.StartCoroutine(LoadingTextCoroutine(label, content));
+		}
+		private static IEnumerator LoadingTextCoroutine(TMP_Text label, string content)
+		{
+			int dotCount = 0;
+			WaitForSeconds delay = new WaitForSeconds(0.5f);
+
+			while (true)
+			{
+				yield return delay;
+				dotCount = (dotCount + 1) % 4;
+				label.text = content + new string('.', dotCount);
+			}
+		}
+	}
+}
