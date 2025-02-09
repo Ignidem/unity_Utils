4dc4252ac2c4686c6e0551d187adcc7ce05749a6
Eivlisskiv <gautma13@gmail.com>
2025-02-09 18:07:52 -0500

    moved label padding animation to a dedicated handler

---
+++ GameObjects/CoroutineInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityUtils.GameObjects
+{
+	public interface ICoroutineHandler
+	{
+		void Stop();
+	}
+
+	public readonly struct CoroutineInfo : ICoroutineHandler
+	{
+		public readonly MonoBehaviour behaviour;
+		public readonly Coroutine coroutine;
+
+		public CoroutineInfo(MonoBehaviour target, IEnumerator routine)
+		{
+			this.behaviour = target;
+			coroutine = target.StartCoroutine(routine);
+		}
+
+		public void Stop()
+		{
+			if (behaviour != null && coroutine != null)
+				behaviour.StopCoroutine(coroutine);
+		}
+	}
+}
---
+++ UI/TMPPro/LoadingTextAnimation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityUtils.GameObjects;
+
+namespace UnityUtils.UI.TMPPro
+{
+	public class LoadingTextAnimation : ICoroutineHandler
+	{
+		public readonly TMP_Text label;
+
+		public string content;
+		public char paddingCharacter;
+		public Range charCount;
+
+		private Coroutine coroutine;
+
+		public LoadingTextAnimation(TMP_Text label, string content, char paddingChar = '.')
+		{
+			this.label = label;
+			this.content = content;
+			this.paddingCharacter = paddingChar;
+			charCount = new Range(1, 3);
+			Start();
+		}
+
+		public void Start()
+		{
+			if (!label.isActiveAndEnabled) return;
+
+			coroutine ??= label.StartCoroutine(LoadingTextCoroutine());
+		}
+
+		public void Stop()
+		{
+			if (coroutine != null)
+			{
+				label.StopCoroutine(coroutine);
+				coroutine = null;
+			}
+		}
+
+		private IEnumerator LoadingTextCoroutine()
+		{
+			int dotCount = 0;
+			WaitForSeconds delay = new WaitForSeconds(0.5f);
+
+			while (true)
+			{
+				yield return delay;
+				dotCount = (dotCount + charCount.Start.Value) % (charCount.End.Value + 1);
+				label.text = content + new string(paddingCharacter, dotCount);
+			}
+		}
+	}
+}
